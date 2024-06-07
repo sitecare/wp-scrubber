06b1717eb47f94bb29da97221a970e213e3eec98
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-07 11:16:03 -0500

    Stubbing out class methods

--- includes/classes/JSONScrubber.php
+++ includes/classes/JSONScrubber.php
@@ -13,4 +13,31 @@ namespace TenUpWPScrubber;
  */
 class JSONScrubber {
 
+	public function __construct() {
+
+	}
+
+	public function scrub_users() {
+
+	}
+
+	public function scrub_post_types() {
+
+	}
+
+	public function scrub_taxonomies() {
+
+	}
+
+	public function scrub_options() {
+
+	}
+
+	public function scrub_custom_tables() {
+
+	}
+
+	public function truncate_tables() {
+
+	}
 }
