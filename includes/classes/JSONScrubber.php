93af4c7f3fbe59d80c9d0e72ed0c230e5d88a95b
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-07 12:10:15 -0500

    Use the config property in methods

--- includes/classes/JSONScrubber.php
+++ includes/classes/JSONScrubber.php
@@ -7,6 +7,8 @@
 
 namespace TenUpWPScrubber;
 
+use WP_CLI;
+
 /**
  * Class JSONScrubber
  * Handles scrubbing for the JSON config.
@@ -33,7 +35,7 @@ class JSONScrubber {
 	 * Scrub user data.
 	 */
 	public function scrub_users() {
-		if ( empty( $config->user_data ) ) {
+		if ( empty( $this->config->user_data ) ) {
 			return;
 		}
 
@@ -41,7 +43,7 @@ class JSONScrubber {
 		$progress = \WP_CLI\Utils\make_progress_bar( 'Scrubbing user data', count( $user_ids ) );
 
 		foreach ( $user_ids as $user_id ) {
-			$scrub = Helpers\scrub_object_by_type( $user_id, $config->user_data, 'user' );
+			$scrub = Helpers\scrub_object_by_type( $user_id, $this->config->user_data, 'user' );
 
 			if ( $show_errors && ( false === $scrub || is_wp_error( $scrub ) ) ) {
 				WP_CLI::error( "Unable to scrub user ID: {$user_id}" );
@@ -57,11 +59,11 @@ class JSONScrubber {
 	 * Scrub post types.
 	 */
 	public function scrub_post_types() {
-		if ( empty( $config->post_types ) ) {
+		if ( empty( $this->config->post_types ) ) {
 			return;
 		}
 
-		foreach ( $config->post_types as $post_type ) {
+		foreach ( $this->config->post_types as $post_type ) {
 			$post_ids = Helpers\get_all_post_ids_of_post_type( $post_type->name );
 			$progress = \WP_CLI\Utils\make_progress_bar( "Scrubbing {$post_type->name} posts", count( $post_ids ) );
 
@@ -98,11 +100,11 @@ class JSONScrubber {
 	 * Scrub taxonomies.
 	 */
 	public function scrub_taxonomies() {
-		if ( empty( $config->taxonomies ) ) {
+		if ( empty( $this->config->taxonomies ) ) {
 			return;
 		}
 
-		foreach ( $config->taxonomies as $taxonomy ) {
+		foreach ( $this->config->taxonomies as $taxonomy ) {
 			$term_ids = Helpers\get_all_term_ids_of_taxonomy( $taxonomy->name );
 			$progress = \WP_CLI\Utils\make_progress_bar( "Scrubbing {$taxonomy->name} terms", count( $term_ids ) );
 
@@ -124,13 +126,13 @@ class JSONScrubber {
 	 * Scrub options.
 	 */
 	public function scrub_options() {
-		if ( empty( $config->options ) ) {
+		if ( empty( $this->config->options ) ) {
 			return;
 		}
 
-		$progress = \WP_CLI\Utils\make_progress_bar( 'Scrubbing options', count( $config->options ) );
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Scrubbing options', count( $this->config->options ) );
 
-		foreach ( $config->options as $option ) {
+		foreach ( $this->config->options as $option ) {
 
 			if ( 'remove' === $option->action ) {
 				$wpdb->delete( $wpdb->options, [ 'option_name' => $option->name ] );
@@ -153,11 +155,11 @@ class JSONScrubber {
 	 * Scrub custom tables.
 	 */
 	public function scrub_custom_tables() {
-		if ( empty( $config->custom_tables ) ) {
+		if ( empty( $this->config->custom_tables ) ) {
 			return;
 		}
 
-		foreach ( $config->custom_tables as $table ) {
+		foreach ( $this->config->custom_tables as $table ) {
 			$name     = $table->name;
 			$pk       = $table->primary_key;
 			$query    = "SELECT {$pk} FROM {$name}";
@@ -168,7 +170,7 @@ class JSONScrubber {
 				$new_data = [];
 
 				foreach ( $table->columns as $field ) {
-					$new_data[ $field->name ] = \TenUpWPScrubber\Helpers\get_field_data_by_action( $field );
+					$new_data[ $field->name ] = Helpers\get_field_data_by_action( $field );
 				}
 
 				$wpdb->update( $name, $new_data, [ $pk => $id ] );
@@ -184,13 +186,13 @@ class JSONScrubber {
 	 * Truncate tables.
 	 */
 	public function truncate_tables() {
-		if ( empty( $config->truncate_tables ) ) {
+		if ( empty( $this->config->truncate_tables ) ) {
 			return;
 		}
 
-		$progress = \WP_CLI\Utils\make_progress_bar( 'Truncating tables', count( $config->truncate_tables ) );
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Truncating tables', count( $this->config->truncate_tables ) );
 
-		foreach ( $config->truncate_tables as $table ) {
+		foreach ( $this->config->truncate_tables as $table ) {
 			$wpdb->query( "TRUNCATE TABLE {$table}" ); // phpcs:ignore WordPress.DB.PreparedSQL
 			$progress->tick();
 		}
