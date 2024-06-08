ae955c893dd7ee738fc4518d65f5981b68d6fe6c
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-07 22:48:03 -0500

    Copying over validation code to validator class methods

--- includes/classes/JSONValidation.php
+++ includes/classes/JSONValidation.php
@@ -39,27 +39,127 @@ class JSONValidation {
 	}
 
 	protected function validate_user_config() {
+		if ( empty( $config->user_data ) ) {
+			return;
+		}
+
+		if ( ! is_object( $config->user_data ) ) {
+			$errors[] = 'Invalid user_data configuration - Must be an object.';
+
+		} else {
+			$errors = array_merge( $errors, validate_object_config( $config->user_data, 'user_data' ) );
+		}
 
 	}
 
 	protected function validate_post_types_config() {
+		if ( empty( $config->post_types ) ) {
+			return;
+		}
 
+		if ( ! is_array( $config->post_types ) ) {
+			$errors[] = 'Invalid post_types configuration - Must be an array.';
+		}
+
+		foreach ( $config->post_types as $post_type ) {
+			if ( ! is_object( $post_type ) ) {
+				$errors[] = 'Invalid post_type configuration - Must be an object.';
+
+			} else {
+				if ( empty( $post_type->name ) ) {
+					$errors[] = 'Invalid post_type configuration - Missing post type name.';
+				}
+
+				$errors = array_merge( $errors, validate_object_config( $post_type, 'post_type' ) );
+			}
+		}
 	}
 
 	protected function validate_taxonomies_config() {
+		if ( empty( $config->taxonomies ) ) {
+			return;
+		}
 
+		if ( ! is_array( $config->taxonomies ) ) {
+			$errors[] = 'Invalid taxonomies configuration - Must be an array.';
+		}
+
+		foreach ( $config->taxonomies as $taxonomy ) {
+			if ( ! is_object( $taxonomy ) ) {
+				$errors[] = 'Invalid taxonomy configuration - Must be an object.';
+
+			} else {
+				if ( empty( $taxonomy->name ) ) {
+					$errors[] = 'Invalid taxonomy configuration - Missing taxonomy name.';
+				}
+
+				$errors = array_merge( $errors, validate_object_config( $taxonomy, 'taxonomy' ) );
+			}
+
+		}
 	}
 
 	protected function validate_options_config() {
+		if ( empty( $config->options ) ) {
+			return;
+		}
 
+		if ( ! is_array( $config->options ) ) {
+			$errors[] = 'Invalid options configuration - Must be an array.';
+		}
+
+		foreach ( $config->options as $option ) {
+			if ( ! is_object( $option ) ) {
+				$errors[] = 'Invalid option configuration - Must be an object.';
+
+			} else {
+				$errors = array_merge( $errors, validate_field_config( $option, 'option' ) );
+			}
+		}
 	}
 
 	protected function validate_custom_tables_config() {
+		if ( empty( $config->custom_tables ) ) {
+			return;
 
+		}
+
+		if ( ! is_array( $config->custom_tables ) ) {
+			$errors[] = 'Invalid custom_tables configuration - Must be an array.';
+		}
+
+		foreach ( $config->custom_tables as $custom_table ) {
+			if ( ! is_object( $custom_table ) ) {
+				$errors[] = 'Invalid custom_table configuration - Must be an object.';
+
+			} else {
+				if ( empty( $custom_table->name ) ) {
+					$errors[] = 'Invalid custom_table configuration - Missing table name.';
+				}
+
+				if ( empty( $custom_table->primary_key ) ) {
+					$errors[] = 'Invalid custom_table configuration - Missing primary key.';
+				}
+
+				$errors = array_merge( $errors, validate_object_config( $custom_table, 'custom_table' ) );
+			}
+		}
 	}
 
 	protected function validate_truncate_config() {
+		if ( empty( $config->truncate_tables ) ) {
+			return;
+		}
 
+		if ( ! is_array( $config->truncate_tables ) ) {
+			$errors[] = 'Invalid truncate_tables configuration - Must be an array.';
+		}
+
+		foreach ( $config->truncate_tables as $truncate_table ) {
+			if ( ! is_string( $truncate_table ) ) {
+				$errors[] = 'Invalid table in truncate_tables - Must be a string.';
+			}
+		}
 	}
 
 }
