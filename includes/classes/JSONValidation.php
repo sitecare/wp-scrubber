60c92b60cbed387b80b13917a98751e16d580e31
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-07 21:58:12 -0500

    Stubbing out class methods

--- includes/classes/JSONValidation.php
+++ includes/classes/JSONValidation.php
@@ -15,4 +15,51 @@ use WP_CLI;
  */
 class JSONValidation {
 
+	/**
+	 * JSON config.
+	 *
+	 * @var object
+	 */
+	protected $config;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param object $config JSON config.
+	 */
+	public function __construct( $config ) {
+		$this->config = $config;
+
+		$this->validate_user_config();
+		$this->validate_post_types_config();
+		$this->validate_taxonomies_config();
+		$this->validate_options_config();
+		$this->validate_custom_tables_config();
+		$this->validate_truncate_config();
+	}
+
+	protected function validate_user_config() {
+
+	}
+
+	protected function validate_post_types_config() {
+
+	}
+
+	protected function validate_taxonomies_config() {
+
+	}
+
+	protected function validate_options_config() {
+
+	}
+
+	protected function validate_custom_tables_config() {
+
+	}
+
+	protected function validate_truncate_config() {
+
+	}
+
 }
