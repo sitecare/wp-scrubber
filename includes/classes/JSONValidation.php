4daf196ca95b0e488d373bd109c9dfd3bf22ba56
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-07 21:51:10 -0500

    Adding validation class

---
+++ includes/classes/JSONValidation.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * JSONValidation class file
+ *
+ * @package TenUpWPScrubber
+ */
+
+namespace TenUpWPScrubber;
+
+use WP_CLI;
+
+/**
+ * Class JSONValidation
+ * Handles validation for the JSON config.
+ */
+class JSONValidation {
+
+}
