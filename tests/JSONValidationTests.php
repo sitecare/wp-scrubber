494d3fe0f85c04652ac8e333a36d678cf48c7d0d
Curtis Loisel <curtis.loisel@get10up.com>
2024-06-24 22:12:38 -0500

    Adding some tests for user config validation

--- tests/JSONValidationTests.php
+++ tests/JSONValidationTests.php
@@ -28,4 +28,43 @@ final class JSONValidationTests extends TestCase {
 		$this->assertEquals( $config->foo, $config_prop->foo );
 		$this->assertEquals( [], $instance->get_errors() );
 	}
+
+	/**
+	 * Tests the validate_user_config method.
+	 * Tests case when no errors are present.
+	 */
+	public function test_validate_user_config_no_errors() {
+		$_config = [
+			'user_data' => [
+				'fields' => [
+					[
+						'name'   => 'display_name',
+						'action' => 'replace',
+						'value'  => 'John Doe',
+					],
+				],
+			],
+		];
+		$config = json_decode( json_encode( $_config ) );
+
+		$validator = new JSONValidation( $config );
+		$errors    = $validator->get_errors();
+
+		$this->assertEmpty( $errors );
+	}
+
+	/**
+	 * Tests the validate_user_config method.
+	 * Tests case when config is not an object.
+	 */
+	public function test_validate_user_config_object_error() {
+		$config = new stdClass();
+		$config->user_data = [ 123 ];
+
+		$validator = new JSONValidation( $config );
+		$errors    = $validator->get_errors();
+
+		$this->assertCount( 1, $errors );
+		$this->assertEquals( 'Invalid user_data configuration - Must be an object.', $errors[0] );
+	}
 }
