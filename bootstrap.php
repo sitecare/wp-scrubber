108724b1ccb54505d5639ac2cefd5b2901a14579
Daryll Doyle <daryll.doyle@10up.com>
2024-11-20 10:58:57 +0000

    Include WP-CLI for testing

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,11 @@
 // First we need to load the composer autoloader, so we can use WP Mock
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Load WordPress and WP-CLI dependencies if needed.
+if ( ! class_exists( 'WP_CLI' ) ) {
+	require_once dirname( __DIR__ ) . '/vendor/wp-cli/wp-cli/php/class-wp-cli.php';
+}
+
 // Use patchwork
 WP_Mock::setUsePatchwork( true );
 
